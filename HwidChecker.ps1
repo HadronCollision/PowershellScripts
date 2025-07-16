79dd2eb6a8d5dca6c52154ba0297ef0da4b73a4f
Abdullah Zafar <102247412+HadronCollision@users.noreply.github.com>
2025-07-17 04:38:57 +0500

    Create HwidChecker.ps1

---
+++ HwidChecker.ps1
@@ -0,0 +1,15 @@
+Clear-Host
+Write-Host "Asian PvP Community HWID Checker" -ForegroundColor Yellow
+Write-Host "Made by " -ForegroundColor DarkGray -NoNewline
+Write-Host "HadronCollision"
+Write-Host ""
+
+$processorId = ""
+$motherboardId = "$((Get-WmiObject win32_baseboard).Manufacturer) $((Get-WmiObject win32_baseboard).Product) $((Get-WmiObject win32_baseboard).SerialNumber)"
+$ramId = "$((Get-WmiObject Win32_PhysicalMemory).Manufacturer) $((Get-WmiObject Win32_PhysicalMemory).PartNumber) $((Get-WmiObject Win32_PhysicalMemory).SerialNumber)"
+$disksId = "$((Get-PhysicalDisk).FriendlyName) $((Get-PhysicalDisk).MediaType) $((Get-PhysicalDisk).Size) $((Get-PhysicalDisk).SerialNumber)"
+
+$hwid = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$motherboardId | $disksId | $ramId")).Replace("=", "") | Set-Clipboard
+Write-Host "HWID copied to clipboard! Paste it into Discord using " -ForegroundColor White -NoNewline
+Write-Host "Ctrl + V" -ForegroundColor Yellow
+Write-Host
