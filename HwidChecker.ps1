a520b420de0132378c198d3666f5d18e35fc0604
Abdullah Zafar <102247412+HadronCollision@users.noreply.github.com>
2025-07-18 07:44:08 +0500

    Update HwidChecker.ps1

--- HwidChecker.ps1
+++ HwidChecker.ps1
@@ -4,12 +4,13 @@ Write-Host "Made by " -ForegroundColor DarkGray -NoNewline
 Write-Host "HadronCollision"
 Write-Host ""
 
-$processorId = "$((Get-WmiObject Win32_Processor).Name)"
 $motherboardId = "$((Get-WmiObject win32_baseboard).Manufacturer) $((Get-WmiObject win32_baseboard).Product) $((Get-WmiObject win32_baseboard).SerialNumber)"
 $ramId = "$((Get-WmiObject Win32_PhysicalMemory).Manufacturer) $((Get-WmiObject Win32_PhysicalMemory).PartNumber) $((Get-WmiObject Win32_PhysicalMemory).SerialNumber)"
 $disksId = "$((Get-PhysicalDisk).FriendlyName) $((Get-PhysicalDisk).MediaType) $((Get-PhysicalDisk).Size) $((Get-PhysicalDisk).SerialNumber)"
+$processorId = "$((Get-WmiObject Win32_Processor).Name)"
 
-$hwid = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$processorId | $motherboardId | $disksId | $ramId")).Replace("=", "") | Set-Clipboard
+$hwid = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("$motherboardId | $ramId | $disksId | $processorId")).Replace("=", "") | Set-Clipboard
+
 Write-Host "HWID copied to clipboard! Paste it into Discord using " -ForegroundColor White -NoNewline
 Write-Host "Ctrl + V" -ForegroundColor Yellow
 Write-Host
